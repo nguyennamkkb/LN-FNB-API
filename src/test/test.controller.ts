673631c776a406bab01567fc13870aded6048918
namnl <>
2023-10-03 00:12:31 +0700

    deploy

--- src/test/test.controller.ts
+++ src/test/test.controller.ts
@@ -1,7 +1,17 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { ResponseHelper } from 'helper/common/response.helper';
+import { ApiResponse } from 'helper/common/response.interface';
 import { Public } from 'src/auth/public.decorator';
 @Controller('test')
 export class TestController {
-    
+    @Get()
+  async findAll(
+   
+  ): Promise<ApiResponse<any>> {
+    try {
+        return ResponseHelper.error(0, "test");
+    } catch (error) {
+      return ResponseHelper.error(0, error);
+    }
+  }
 }
