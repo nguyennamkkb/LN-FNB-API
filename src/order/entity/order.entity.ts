7306999dbdb03bff07a1eb6051de792451730bb7
namnl <>
2023-10-15 17:44:02 +0700

    them thoi gian vao bang order

--- src/order/entity/order.entity.ts
+++ src/order/entity/order.entity.ts
@@ -28,6 +28,9 @@ export class OrderEntity {
   status: number;
 
   @Column({ type: 'bigint' })
+  time: number;
+
+  @Column({ type: 'bigint' })
   createAt: number;
 
   @Column({ type: 'bigint' })
