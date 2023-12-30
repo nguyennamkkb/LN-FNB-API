9cd6796002dc07666ff90f9de17ab197cc402752
Your Name <you@example.com>
2023-12-30 15:55:49 +0700

    s

--- helper/common/logger.ts
+++ helper/common/logger.ts
@@ -4,17 +4,22 @@ import { Common } from './common';
 
 export  function writeLogToFile(log: string){
     //   const logFilePath = path.join(__dirname, 'logs', 'app.log');
-    const timeNow: String = Common.getCurrentTime()
-    const fileLogName: String = timeNow.substring(0,10)
-    const logFilePath = `log/${fileLogName}.txt`;
-
-    // Create the logs directory if it doesn't exist
-    if (!fs.existsSync(path.dirname(logFilePath))) {
-        fs.mkdirSync(path.dirname(logFilePath), { recursive: true });
+    try {
+        const timeNow: String = Common.getCurrentTime()
+        const fileLogName: String = timeNow.substring(0,10)
+        const logFilePath = `log/${fileLogName}.txt`;
+    
+        // Create the logs directory if it doesn't exist
+        if (!fs.existsSync(path.dirname(logFilePath))) {
+            fs.mkdirSync(path.dirname(logFilePath), { recursive: true });
+        }
+    
+        // Append the log to the file
+    
+        const headLog = timeNow + " " + Common.AppName + ": "
+         fs.appendFileSync(logFilePath, headLog + log + '\n');
+    } catch (error) {
+        
     }
-
-    // Append the log to the file
-
-    const headLog = timeNow + " " + Common.AppName + ": "
-     fs.appendFileSync(logFilePath, headLog + log + '\n');
+   
 }
